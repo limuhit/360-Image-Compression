7fd7c21c5e39db2c29266b520652bff6e496d2d6
Mu Li <limuhit@gmail.com>
2024-08-29 10:45:09 +0800

    Add new setup file for cuda 12.1 + pytorch 2.3

---
+++ setup_new.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import os
+import torch
+
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+			
+cxx_args = ['-std=c++17',"-O3"] # test on cuda 12.1 + pytorch 2.3
+nvcc_args = [
+	'-D__CUDA_NO_HALF_OPERATORS__',
+    '-gencode', 'arch=compute_60,code=sm_60', # change according to https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards/
+    '-gencode', 'arch=compute_61,code=sm_61',
+    '-gencode', 'arch=compute_75,code=sm_75',
+	'-gencode', 'arch=compute_80,code=sm_80',
+	'-gencode', 'arch=compute_86,code=sm_86'
+]
+
+setup(
+    name='lic360',
+    packages=['lic360_operator'],
+    ext_modules=[
+        CUDAExtension('lic360', [
+            './extension/main.cpp',
+            './extension/math_cuda.cu',
+            './extension/projects_cuda.cu',
+            './extension/imp_map_cuda.cu',
+            './extension/dtow_cuda.cu',
+            './extension/quant_cuda.cu',
+            './extension/sphere_pad_cuda.cu',
+			'./extension/sphere_trim_cuda.cu',
+            './extension/sphere_cut_edge_cuda.cu',
+            './extension/sphere_lat_scale_cuda.cu',
+			'./extension/contex_shift_cuda.cu',
+			'./extension/context_reshape_cuda.cu',
+			'./extension/entropy_gmm_cuda.cu',
+			'./extension/mask_constrain_cuda.cu',
+			'./extension/code_contex_cuda.cu',
+			'./extension/cconv_dc_cuda.cu',
+			'./extension/cconv_ec_cuda.cu',
+			'./extension/tile_extract_cuda.cu',
+			'./extension/tile_input_cuda.cu',
+			'./extension/tile_add_cuda.cu',
+			'./extension/entropy_gmm_table_cuda.cu',
+            './extension/coder.cpp',
+            './extension/ArithmeticCoder.cpp',
+            './extension/BitIoStream.cpp',
+			'./extension/dquant_cuda.cu',
+			'./extension/entropy_table_cuda.cu',
+			'./extension/scale_cuda.cu',
+			'./extension/imp2mask_cuda.cu',
+			'./extension/CPP_cuda.cu',
+            './extension/viewport_cuda.cu',
+        ],
+        include_dirs=['./extension'], 
+        extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args}, 
+        libraries=['cublas'])
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
